b011fbd712ae6203524d3aff9dfd305f2e4f84cc
Cecile.LAROCHE <11458@holbertonstudents.com>
2025-10-27 22:54:16 +0100

    build: :hammer: TASK 0

--- part3/config.py
+++ part3/config.py
@@ -1,12 +1,18 @@
 import os
 
+
 class Config:
+    """Base configuration"""
     SECRET_KEY = os.getenv('SECRET_KEY', 'default_secret_key')
     DEBUG = False
 
+
 class DevelopmentConfig(Config):
+    """Configuration pour le développement"""
     DEBUG = True
 
+
+# Dictionnaire des configurations disponibles
 config = {
     'development': DevelopmentConfig,
     'default': DevelopmentConfig
--- part3/run.py
+++ part3/run.py
@@ -1,6 +1,8 @@
 from app import create_app
 
-app = create_app()
+# Crée l'application avec la configuration de développement
+app = create_app('development')
+# charge DevelopmentConfig depuis le dict config
 
 if __name__ == '__main__':
     app.run(debug=True)
